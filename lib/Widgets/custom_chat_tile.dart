a16b13a27f65e03abb88192d70dd093f4fc424c4
MuhammadUzain55 <uzainsheikh55@gmail.com>
2025-03-11 02:12:50 +0500

    Updated

--- lib/Widgets/custom_chat_tile.dart
+++ lib/Widgets/custom_chat_tile.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 
 class CustomChatTile extends StatelessWidget {
-  const CustomChatTile({super.key});
+  final String name;
+  final String message;
+  final String? time;
+  const CustomChatTile({
+    super.key,
+    required this.name,
+    required this.message,
+    this.time,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 2),
-      child: const ListTile(
+      child: ListTile(
         tileColor: Colors.lightGreen,
-        leading: CircleAvatar(
+        leading: const CircleAvatar(
           backgroundColor: Colors.white,
           radius: 25,
         ),
-        title: Text("Uzain sheikh"),
-        subtitle: Text("mai a raha hu"),
-        trailing: Text("9:00 am"),
+        title: Text(name),
+        subtitle: Text(message),
+        trailing: Text(time ?? ""),
       ),
     );
   }
+
+  
 }
